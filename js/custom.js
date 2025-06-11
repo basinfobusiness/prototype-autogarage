f3748b79b6ee4920bda9937fdeb389978f782f7a
infospot <infospot112@gmail.com>
2025-06-11 17:43:34 +0000

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // to get current year
 function getYear() {
     var currentDate = new Date();
@@ -8,5 +10,24 @@ function getYear() {
 getYear();
 
 (()=>{
-    alert("You can click on some of the buttons and see it's action. Example, Try clicking on the call button at the footer.")
+    var userResponse = prompt("Have you gone through the buttons","Reply with Y/N");
+    if (userResponse === "Y") {
+        alert('Continue browsing through the other pages.');
+    }
+    else if (userResponse === "Reply with Y/N") {
+        alert('Please answer with either Y for Yes or N for No');
+        window.location.reload();
+    }else if(userResponse === "N"){
+        var user = confirm("You can click on some of the buttons and see it's action. Example, Try clicking on the call button at the footer. Take me there?")
+        if (user) {
+            // alert('Taking you there')
+            window.location.href = '#contact-us';
+        }else{
+            alert('Browse through the pages to catch a glimpse');
+        }
+    }
+    else{
+        alert('something went wrong.');
+    }
+    // alert("You can click on some of the buttons and see it's action. Example, Try clicking on the call button at the footer.")
 })();
